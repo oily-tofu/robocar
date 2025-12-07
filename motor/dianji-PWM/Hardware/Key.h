66bba38e4c3c2cf0f7678c52b358295ee11a42a1
oily-tofu <165025031+oily-tofu@users.noreply.github.com>
2025-12-07 18:36:46 +0800

    Delete motor directory

--- motor/dianji-PWM/Hardware/Key.h
+++
@@ -1,9 +0,0 @@
-#ifndef __KEY_H
-#define __KEY_H
-
-void Key_Init(void);
-uint8_t Key_GetNum(void);
-void TIM6_Update_Init(void);
-void PIDset(void);
-
-#endif
--- motor/dianji-PWM/Hardware/PWM.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PWM_H
-#define __PWM_H
-
-void PWM_Init(void);
-void PWM_SetCompare1(uint16_t Compare);
-
-void TIM5_Update_Init(void);
-
-#endif
